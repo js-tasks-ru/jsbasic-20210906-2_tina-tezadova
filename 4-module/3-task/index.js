d4f149e37367de20c25c54ec2fa4719fd3faf91c
Tina Tezadova <f.tezadova@tinkoff.ru>
2021-09-21 13:16:38 +0300

    Занятие 4: Знакомство с ДОМ

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,50 @@
 function highlight(table) {
-  // ваш код...
+  let row = table.rows;
+  let positionGender;
+  let positionAge;
+  let positionStatus;
+
+  for (let i = 0; i < row[0].cells.length; i++) {
+    if (row[0].cells[i].innerHTML === 'Age') {
+      positionAge = i;
+
+    }
+    if (row[0].cells[i].innerHTML === 'Gender') {
+      positionGender = i;
+
+    }
+    if (row[0].cells[i].innerHTML === 'Status') {
+      positionStatus = i;
+
+    }
+  }
+
+  let Container = {
+    m: 'male',
+    f: 'female',
+    true: 'available',
+    false: 'unavailable'
+  };
+
+  for (let j = 1; j < row.length; j++) {
+    let gender = row[j].children[positionGender];
+    
+    gender.parentNode.classList.add(Container[gender.innerHTML]);
+
+    let age = row[j].children[positionAge].innerHTML;
+    
+    if (age < 18) {
+      gender.parentNode.setAttribute('style', 'text-decoration: line-through');
+    }
+
+    let status = row[j].children[positionStatus];
+    
+    if (status.hasAttribute('data-available')) {
+      let elem = status.getAttribute('data-available');
+      
+      status.parentNode.classList.add(Container[elem]);
+    } else {
+      status.parentNode.hidden = true;
+    }
+  }
 }
