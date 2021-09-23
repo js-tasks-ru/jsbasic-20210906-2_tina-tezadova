ffad4bfa0743edf2e53ce55ccc37fd36fa2f4e54
Tina Tezadova <f.tezadova@tinkoff.ru>
2021-09-23 15:57:55 +0300

    исправление замечаний

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,6 +3,12 @@ function highlight(table) {
   let positionGender;
   let positionAge;
   let positionStatus;
+  const elementValueMap = {
+    m: 'male',
+    f: 'female',
+    true: 'available',
+    false: 'unavailable'
+  };
 
   for (let i = 0; i < row[0].cells.length; i++) {
     if (row[0].cells[i].innerHTML === 'Age') {
@@ -19,30 +25,23 @@ function highlight(table) {
     }
   }
 
-  let Container = {
-    m: 'male',
-    f: 'female',
-    true: 'available',
-    false: 'unavailable'
-  };
-
-  for (let j = 1; j < row.length; j++) {
-    let gender = row[j].children[positionGender];
+  for (let i = 1; i < row.length; i++) {
+    let gender = row[i].children[positionGender];
     
-    gender.parentNode.classList.add(Container[gender.innerHTML]);
+    gender.parentNode.classList.add(elementValueMap[gender.innerHTML]);
 
-    let age = row[j].children[positionAge].innerHTML;
+    let age = row[i].children[positionAge].innerHTML;
     
     if (age < 18) {
       gender.parentNode.setAttribute('style', 'text-decoration: line-through');
     }
 
-    let status = row[j].children[positionStatus];
+    let status = row[i].children[positionStatus];
     
     if (status.hasAttribute('data-available')) {
       let elem = status.getAttribute('data-available');
       
-      status.parentNode.classList.add(Container[elem]);
+      status.parentNode.classList.add(elementValueMap[elem]);
     } else {
       status.parentNode.hidden = true;
     }
