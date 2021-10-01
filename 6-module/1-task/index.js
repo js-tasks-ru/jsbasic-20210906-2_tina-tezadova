fb46d77d3596ace21cbc2e81901e39424a8775ee
Tina Tezadova <f.tezadova@tinkoff.ru>
2021-10-01 16:10:07 +0300

    Занятие 6: Классы и компоненты (1 Таблица с удаляемыми строками)

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,57 @@
  *      }
  *
  */
-export default class UserTable {
-  constructor(rows) {
-  }
+
+function TableComponentTemplate(items) {
+  let table = document.createElement("table");
+  let result = `
+  <thead>
+      <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+      </tr>
+  </thead>
+  <tbody>
+      
+      ${items.map((item) =>
+    `<tr>
+    <td>${item.name}</td> 
+    <td>${item.age}</td>
+    <td>${item.salary}</td>
+    <td>${item.city}</td>
+    <td><button>X</button></td>
+    </tr>`).join('')}
+      </tr>
+      </tbody>
+  `;
+  table.innerHTML = result;
+  
+  const removeButton = Array.from(table.querySelectorAll('button'));
+  
+  removeButton.forEach((btn)=> {
+    btn.onclick = function(event) {
+      let target = event.target;
+
+      target.parentNode.parentNode.remove();
+      console.log(target.parentNode);
+    };
+  });
+  
+  return table;
 }
+export default class UserTable {
+  #elem = '';
+  constructor(rows) {
+    this.#elem = new TableComponentTemplate(rows);
+  }
+ 
+  get elem() {
+    return this.#elem;
+  }
+  
+
+}
+
